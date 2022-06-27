0ab368ceaad5ff10ad631bd655b9040ea475b3d9
Julien Overholtzer <47stilldreaming@gmail.com>
2022-06-27 23:16:58 +0200

    Co-authored-by: mateo.le.duc@outlook.fr <mateo.le.duc@outlook.fr> Co-authored-by: barriere_benjamin@yahoo.fr <barriere_benjamin@yahoo.fr>

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,27 @@
 import React from "react";
-// import {Navlink} from 'react-rooter-dom';
 
 const Nav = () => {
-    return <nav>
-            <ul>
-                <li><i class="fas fa-home"></i></li>
-                <li><i class="fas fa-calendar-alt"></i></li>
-                <li><i class="fas fa-microphone-alt"></i></li>
-                <li><i class="fas fa-drum"></i></li>
-                <li><i class="fas fa-users"></i></li>
-            </ul>
-        </nav>;
-  };
 
-  export default Nav
+    const navItems = [
+        "fas fa-home",
+        "fas fa-calendar-alt",
+        "fas fa-microphone-alt",
+        "fas fa-drum",
+        "fas fa-users"
+    ];
+
+    const ItemsComponents = ({ cls }) => <li><i className={cls}></i></li>;
+
+    return (
+        <nav>
+            <ul>
+                {
+                    navItems.map((classAttribute, idx) => <ItemsComponents cls={classAttribute} key={idx}></ItemsComponents>)
+                }
+            </ul>
+        </nav>
+    )
+};
+
+
+export default Nav;
