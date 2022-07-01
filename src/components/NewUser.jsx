ebc09a089abdc0152797a78f446f3fa7d0bfda87
MateoLDC <leducmateo@gmail.com>
2022-07-01 17:28:08 +0200

    Pages User et NewUser avec Style

--- src/components/NewUser.jsx
+++ src/components/NewUser.jsx
@@ -1,23 +1,23 @@
+import "./_newUser.scss";
+
 const NewUser = () => {
   return (
-    <div>
-      <h2>Ajouter un nouvel utilisateur</h2>
-      <form action="">
-        <label htmlFor="">Prénom :</label>
-        <input type="text" />
-
-        <label htmlFor="">Nom :</label>
-        <input type="text" />
-
-        <label htmlFor="">Email :</label>
-        <input type="mail" />
-
-        <label htmlFor="">Mot de passe :</label>
-        <input type="text" />
-
-        <button type="submit">Ajouter</button>
-      </form>
-    </div>
+    <>
+      <div className="container">
+        <h2>Ajouter un nouvel utilisateur</h2>
+        <form action="">
+          <label htmlFor="">Prénom :</label>
+          <input type="text" />
+          <label htmlFor="">Nom :</label>
+          <input type="text" />
+          <label htmlFor="">Email :</label>
+          <input type="mail" />
+          <label htmlFor="">Mot de passe :</label>
+          <input type="text" />
+          <button type="submit">Ajouter</button>
+        </form>
+      </div>
+    </>
   );
 };
 
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,12 +1,22 @@
-// import Header from "../layouts/Header";
-// import Nav from "../layouts/Nav";
-// import Main from "../layouts/Main";
 import { NavLink } from "react-router-dom";
+
+import "./_user.scss";
 
 const User = () => {
   return (
     <>
       <div className="container">
+        <div className="card">
+          <h4>Nom User</h4>
+          <ul className="mod-sup">
+            <li className="mod">
+              <i class="fas fa-edit"></i>
+            </li>
+            <li className="sup">
+              <i class="fas fa-trash-alt"></i>
+            </li>
+          </ul>
+        </div>
         <div className="card">
           <h4>Nom User</h4>
           <ul className="mod-sup">
