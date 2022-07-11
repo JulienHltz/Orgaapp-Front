ef5dc358354896da51f34f13cafa847e987329c7
MateoLDC <leducmateo@gmail.com>
2022-07-11 10:32:44 +0200

    react hook form et yup

--- src/components/NewUser.jsx
+++ src/components/NewUser.jsx
@@ -1,24 +1,71 @@
-import "./_newUser.scss";
+import './_newUser.scss'
+import axios from 'axios'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+const schema = yup.object().shape({
+  username: yup
+    .string()
+    .required()
+    .min(3, 'Minimum 3 caracteres !!')
+    .max(15)
+})
 
 const NewUser = () => {
+  const {
+    register,
+    handleSubmit,
+    formState,
+    formState: { errors }
+  } = useForm({
+    mode: 'onTouched',
+    resolver: yupResolver(schema)
+  })
+
+  const { isSubmitting } = formState
+
+  const onSubmit = data => {
+    data.roles = [data.roles]
+    console.log(data)
+    axios
+      .post('http://localhost:5050/api/users', data)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(e => {
+        console.log(e.code)
+      })
+  }
+
   return (
     <>
-      <div className="container">
+      <div className='container'>
         <h2>Ajouter un nouvel utilisateur</h2>
-        <form action="">
-          <label htmlFor="">Prénom :</label>
-          <input type="text" />
-          <label htmlFor="">Nom :</label>
-          <input type="text" />
-          <label htmlFor="">Email :</label>
-          <input type="mail" />
-          <label htmlFor="">Mot de passe :</label>
-          <input type="text" />
-          <button type="submit">Ajouter</button>
+        <form action='' onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor='username'>Nom d'utilisateur :</label>
+          <input type='text' id='username' {...register('username')} />
+          {errors.username && <span>{errors.username.message}</span>}
+          <label htmlFor='firstname'>Prénom :</label>
+          <input type='text' id='firstname' {...register('firstname')} />
+          <label htmlFor='lastname'>Nom :</label>
+          <input type='text' id='lastname' {...register('lastname')} />
+          <label htmlFor='role'>role</label>
+          <select name='role' id='roles' {...register('roles')}>
+            <option value='User'>User</option>
+            <option value='Admin'>Admin</option>
+          </select>
+          <label htmlFor='email'>Email :</label>
+          <input type='mail' id='email' {...register('email')} />
+          <label htmlFor='password'>Mot de passe :</label>
+          <input type='text' id='password' {...register('password')} />
+          <button type='submit' disabled={isSubmitting}>
+            Ajouter
+          </button>
         </form>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default NewUser;
+export default NewUser
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,39 +1,39 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom'
 
-import "./_user.scss";
+import './_user.scss'
 
 const User = () => {
   return (
     <>
-      <div className="container">
-        <div className="card">
+      <div className='container'>
+        <div className='card'>
           <h4>Nom User</h4>
-          <ul className="mod-sup">
-            <li className="mod">
-              <i class="fas fa-edit"></i>
+          <ul className='mod-sup'>
+            <li className='mod'>
+              <i className='fas fa-edit'></i>
             </li>
-            <li className="sup">
-              <i class="fas fa-trash-alt"></i>
+            <li className='sup'>
+              <i className='fas fa-trash-alt'></i>
             </li>
           </ul>
         </div>
-        <div className="card">
+        <div className='card'>
           <h4>Nom User</h4>
-          <ul className="mod-sup">
-            <li className="mod">
-              <i class="fas fa-edit"></i>
+          <ul className='mod-sup'>
+            <li className='mod'>
+              <i className='fas fa-edit'></i>
             </li>
-            <li className="sup">
-              <i class="fas fa-trash-alt"></i>
+            <li className='sup'>
+              <i className='fas fa-trash-alt'></i>
             </li>
           </ul>
         </div>
-        <NavLink to="/nouvel-utilisateur">
-          <i class="fas fa-plus-circle fa-3x add"></i>
+        <NavLink to='/nouvel-utilisateur'>
+          <i className='fas fa-plus-circle fa-3x add'></i>
         </NavLink>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default User;
+export default User
